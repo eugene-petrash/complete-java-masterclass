87bb738846629029df0a4ca4cd8b549a6b1a5a7f
Eugene Petrash <petrash.evgeniy@gmail.com>
2018-02-10 22:14:12 +0200

    Use variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 10;
+        int myAdditionResult = myFirstNumber + mySecondNumber;
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
+        System.out.println(myAdditionResult);
+        System.out.println("myAdditionResult");
     }
 
 }

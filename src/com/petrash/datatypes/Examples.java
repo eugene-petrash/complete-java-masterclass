091c5b7483c993aa21cfd90569c6530107692acf
Eugene Petrash <petrash.evgeniy@gmail.com>
2018-02-11 14:26:00 +0200

    Primitive data types - The Float and Double

---
+++ src/com/petrash/datatypes/Examples.java
@@ -0,0 +1,15 @@
+package com.petrash.datatypes;
+
+/**
+ * Created by petrash on 2/11/18.
+ */
+public class Examples {
+
+    public static void main(String[] args) {
+        int poundsValue = 3;
+        double kilogramResult = poundsValue * 0.45359237;
+        System.out.println(poundsValue + " pounds are equal to " + kilogramResult + " kilogram");
+    }
+
+
+}
--- src/com/petrash/datatypes/PrimitiveDataTypes.java
+++ src/com/petrash/datatypes/PrimitiveDataTypes.java
@@ -7,25 +7,39 @@ public class PrimitiveDataTypes {
 
     public static void main(String[]  args) {
 
-        // has a width of 64
+        /*
+        "Whole" numbers
+         */
+
+        // has a width of 64 (8 bytes)
         long minLongValue = -9_223_372_036_854_775_808L;
         long maxLongValue = 9_223_372_036_854_775_807L;
         long newLongValue = maxLongValue / 2;
 
-        // has a width of 32
+        // has a width of 32 (4 bytes)
         int minIntValue = -2_147_483_648;
         int maxIntValue = 2_147_483_647;
         int newIntValue = minIntValue *3;
 
-        // has a width of 16
+        // has a width of 16 (2 bytes)
         short minShortValue = -32768;
         short maxShortValue = 32767;
         short newShortValue = (short) (minShortValue + 145);
 
-        // has a width of 8
+        // has a width of 8 (1 byte)
         byte minByteValue = -128;
         byte maxByteValue = 127;
         byte newByteValue = (byte) (maxByteValue - 67);
+
+        /*
+        Numbers with decimal points
+         */
+
+        // has a width of 64 (8 bytes)
+        double doubleValue =1.7976931348623157E308d;
+
+        // has a width of 32 (4 bytes)
+        float floatValue =3.4028235E38f;
 
     }
 }
